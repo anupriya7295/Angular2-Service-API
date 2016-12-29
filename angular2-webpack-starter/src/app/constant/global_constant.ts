3090537f0d03dfc496a976576a7ffc45fdd1052a
anupriya72 <anupriya7295@gmail.com>
2016-12-29 13:30:51 +0530

    changed return format for url in global_constant

--- angular2-webpack-starter/src/app/constant/global_constant.ts
+++ angular2-webpack-starter/src/app/constant/global_constant.ts
@@ -13,4 +13,4 @@
  */
 'use strict';
 export var baseUrl: string = "http://demo.radaptive.com:7090/api?callback=JSONP_CALLBACK&action=query&username=superuser&password=superuser&queryName=GET_PROJECTS_BY_STATUS&status=Closed&retFormat=json&keyValuePair=yes&endLimit=100";
-export var url: string = "http://demo.radaptive.com:7090/api?callback=JSONP_CALLBACK&action=create&username=superuser&password=superuser&app=Users&"
+export var url: string = "http://demo.radaptive.com:7090/api?callback=JSONP_CALLBACK&action=create&username=superuser&password=superuser&app=Users&retFormat=json&keyValuePair=yes&"
